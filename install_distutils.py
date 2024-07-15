d39a70b7ef7377d1aad5673f4f01573861a85f82
Nor Anisa <48305341+NourAnisa@users.noreply.github.com>
2024-07-15 12:00:30 +0700

    Create install_distutils.py

---
+++ install_distutils.py
@@ -0,0 +1,10 @@
+import subprocess
+
+def install_distutils():
+    try:
+        import distutils
+    except ImportError:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "setuptools"])
+
+if __name__ == "__main__":
+    install_distutils()
